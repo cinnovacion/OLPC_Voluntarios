0aca4edbc7d9f4daa44418eca925c13d65f35c28
Vince Dobbelaere <vince.dobbelaere@kahosl.be>
2016-05-09 14:04:58 -0600

    bugfix

--- src/ZamoraTeran/Provider/Controller/ajaxController.php
+++ src/ZamoraTeran/Provider/Controller/ajaxController.php
@@ -122,8 +122,8 @@ class ajaxController implements ControllerProviderInterface {
 			
 			$trabaja = array(
 				'Persona_idPersona' => $search['id'],
-				'horaInicio' => $search['entrada'],
-				'horaFinal' => $search['salida'],
+				'horaInicio' =>  date("H:i", strtotime($search['entrada'])),
+				'horaFinal' => date("H:i", strtotime($search['salida'])),
 				'dia' => implode('/', array_reverse(explode('/', $search['dia']))),
 				'tiempo' => null
 				);
@@ -135,16 +135,7 @@ class ajaxController implements ControllerProviderInterface {
 				)/(3600), 2);
 
 
-			if(substr($search['entrada'], -2) == "PM"){
-				$trabaja['horaInicio'] =  date('h:i:s', strtotime($search['entrada'])+43200);
-				$search['entrada'] =  "changed time";
-			}
-
-			if(substr($search['salida'], -2) == "PM"){
-				$trabaja['horaFinal'] = date('h:i:s', strtotime($search['salida'])+43200);
-
-				$search['salida'] =  "changed time";
-			}
+			
 
 			$app['db.trabajar']->insert($trabaja);
 			echo json_encode($search);
